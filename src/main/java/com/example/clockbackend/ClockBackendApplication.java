ca065138965659b79a8b464975019a76c5b678a0
nati schmidt <nati.schmidt97@outlook.de>
2023-04-05 13:28:59 +0200

    Initial commit

---
+++ src/main/java/com/example/clockbackend/ClockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.clockbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClockBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clockbackend/ClockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clockbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
