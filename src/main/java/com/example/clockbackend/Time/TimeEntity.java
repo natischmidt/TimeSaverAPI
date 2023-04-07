455ab49397c70a5afe2e376a9a9041d58b9814d9
nati schmidt <nati.schmidt97@outlook.de>
2023-04-07 12:13:54 +0200

    table to save times WIP

--- src/main/java/com/example/clockbackend/Time/TimeEntity.java
+++ src/main/java/com/example/clockbackend/Time/TimeEntity.java
@@ -1,14 +1,23 @@
 package com.example.clockbackend.Time;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
-
+@Entity(name = "time")
+@Table(name = "Time")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 
 public class TimeEntity {
 
     //need an id to delete specific time later on
+    @Id
+    @Column(name = "id")
     private String id;
+    @Column(name = "time")
     private float time;
 
 }
