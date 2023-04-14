a78a11641d3c8eb006cd1f84e6de62e194662d6a
nati schmidt <nati.schmidt97@outlook.de>
2023-04-14 20:11:42 +0200

    cant retrieve info WIP

--- src/main/java/com/example/clockbackend/Time/TimeController.java
+++ src/main/java/com/example/clockbackend/Time/TimeController.java
@@ -1,6 +1,9 @@
 package com.example.clockbackend.Time;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/timer")
@@ -10,13 +13,21 @@ public class TimeController {
 
     //To get all the times saved
     @CrossOrigin
-    @PostMapping
+    @PostMapping()
     public DTO Saved(@RequestBody DTO dto) {
         return timeService.Save(dto)
                 .map(TimeController::timesdto)
                 .orElse(null);
     }
 
+    @CrossOrigin
+    @GetMapping("times")
+    public List<DTO> GetTimes() {
+        return timeService.all()
+                .stream()
+                .map(TimeController::timesdto)
+                .collect(Collectors.toList());
+    }
 
     private static DTO timesdto(TimeEntity timeEntity) {
         return new DTO(
--- src/main/java/com/example/clockbackend/Time/TimeRepo.java
+++ src/main/java/com/example/clockbackend/Time/TimeRepo.java
@@ -1,14 +1,9 @@
 package com.example.clockbackend.Time;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Stream;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface TimeRepo extends JpaRepository<TimeEntity, String> {
 
 
